f35d78bdaf9019a2d3ed37ceea3ae593b4f96787
Ameer <ameerapex496@gmail.com>
2023-11-24 18:26:37 +0100

    added function and some tyling error fix

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
-console.log("hallo world");
+// selecting our elements
+var openBtns = document.querySelectorAll(".open-Close-Btn");
+var collapWraps = document.querySelectorAll(".collap-Wrap");
+// our functions
+var openHandler = function (e) {
+    // getting target button
+    var btn = e.currentTarget;
+    // getting parrents of button
+    var collapWrap = btn.parentElement.parentElement;
+    var iEl = btn.querySelector("i");
+    // if contains that class or not
+    if (!collapWrap.classList.contains("active")) {
+        collapWrap.classList.add("active");
+        iEl.classList.replace("fa-caret-down", "fa-xmark");
+        btn;
+    }
+    else {
+        collapWrap.classList.remove("active");
+        iEl.classList.replace("fa-xmark", "fa-caret-down");
+    }
+};
+// our event listeners
+openBtns.forEach(function (btn) {
+    btn.addEventListener("click", openHandler);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,27 @@
-console.log("hallo world");
+// selecting our elements
+const openBtns = document.querySelectorAll(".open-Close-Btn");
+const collapWraps = document.querySelectorAll(".collap-Wrap");
+
+// our functions
+const openHandler = (e) => {
+  // getting target button
+  const btn = e.currentTarget;
+  // getting parrents of button
+  const collapWrap = btn.parentElement.parentElement;
+  const iEl = btn.querySelector("i");
+
+  // if contains that class or not
+  if (!collapWrap.classList.contains("active")) {
+    collapWrap.classList.add("active");
+    iEl.classList.replace("fa-caret-down", "fa-xmark");
+    btn;
+  } else {
+    collapWrap.classList.remove("active");
+    iEl.classList.replace("fa-xmark", "fa-caret-down");
+  }
+};
+
+// our event listeners
+openBtns.forEach((btn) => {
+  btn.addEventListener("click", openHandler);
+});
